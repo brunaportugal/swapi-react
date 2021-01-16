57a28f51dc68efedd8e6a97afd645b4b890b7ed7
Bruna Portugal <brunabportugal@gmail.com>
2021-01-16 13:26:47 +0100

    added characters

--- src/components/Film.js
+++ src/components/Film.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Grid, Menu, Container } from 'semantic-ui-react';
 import { Link, Route, useLocation } from 'react-router-dom';
 
-export default function Film({ data }) {
+export default function Film() {
   const location = useLocation();
   console.log(location);
   const film = location.state;
+  console.log(film);
+
+  const numberPattern = /\d+/;
+
+  const characterIds = film.characters.map(character => character.match( numberPattern ) [0] );
+  console.log(characterIds);
+
+
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function fetchCharacters() {
+      const characterInfo = await Promise.all(characterIds.map(async id => {
+        let res = await fetch(`https://swapi.dev/api/people/${id}/`);
+        let characterData = await res.json();
+        characterData.id = id;
+        return characterData;
+      }))
+      setCharacters(characterInfo);
+      console.log(characterInfo);
+    }
+
+    fetchCharacters();
+  }, [])
+
   return (
     <>
       <h1>{film.title}</h1>
@@ -14,6 +39,13 @@ export default function Film({ data }) {
           <Link to={`/film/${film.episode_id}`}>
             <img className="films-images" src={`../films/${film.episode_id}.jpg`} alt=""></img>
           </Link>
+          <ul>
+            {characters.map(character =>
+              <li>
+                <img className="avatar" src={`../characters/${character.id}.jpg`} alt=""></img>
+                {character.name}
+              </li>)}
+          </ul>
         </Grid.Column>
       </Grid>
     </>
